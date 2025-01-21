5a5820b2f24a5ff52c6640d587d0f0aed204ccf7
Laura Beatriz <162806846+laubtrizz@users.noreply.github.com>
2025-01-21 09:18:35 -0300

    Update q04.c

--- q04.c
+++ q04.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-//Um feirista vende suas maças a R$ 1,30 por unidade, se forem compradas menos de
-//uma dúzia, e a R$ 1,10 por unidade, se forem compradas pelo menos uma dúzia.
-//Sabendo disso, crie um programa que solicita e lê a quantidade de maças desejadas,
-//calcula e informa o preço a ser pago pelo cliente, considerando os preços definidos anteriormente.
+/*Um feirista vende suas maças a R$ 1,30 por unidade, se forem compradas menos de
+uma dúzia, e a R$ 1,10 por unidade, se forem compradas pelo menos uma dúzia.
+Sabendo disso, crie um programa que solicita e lê a quantidade de maças desejadas,
+calcula e informa o preço a ser pago pelo cliente, considerando os preços definidos anteriormente.*/
 
 int main(){
     
