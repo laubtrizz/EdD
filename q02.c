6085cf1a75eb0f5972b5fda685d5ee2649f7ff24
Laura Beatriz <162806846+laubtrizz@users.noreply.github.com>
2025-01-21 09:17:50 -0300

    Update q02.c

--- q02.c
+++ q02.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-//Crie um programa que solicita e lê dois números inteiros (numerador e denominador)
-//e verifica se a divisão é exata. Se for, exiba a mensagem: “É divisível!”.
-//Caso contrário, exiba a mensagem: “Não é divisível!”.
-//Obs.: não esqueça que a divisão só deve ser realizada, caso o denominador seja diferente de zero.
-//Caso contrário, o programa deve exibir uma mensagem informando que a divisão é indefinida.
+/*Crie um programa que solicita e lê dois números inteiros (numerador e denominador)
+e verifica se a divisão é exata. Se for, exiba a mensagem: “É divisível!”.
+Caso contrário, exiba a mensagem: “Não é divisível!”.
+Obs.: não esqueça que a divisão só deve ser realizada, caso o denominador seja diferente de zero.
+Caso contrário, o programa deve exibir uma mensagem informando que a divisão é indefinida.*/
 
 int main(){
     int n, d;
